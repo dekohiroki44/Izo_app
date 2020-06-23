fab0a2fb9fd23a6590a71e2f321994578b5de6f5
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-06-23 23:26:12 +0900

    home_spec

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "home", type: :request do
+  let!(:live) { build(:live) }
+  let!(:post) { build(:post) }
+
+  before do
+    get root_path
+  end
+
+  it 'shows link' do
+    expect(page).to have_link 'Izo Official Web Site', href: root_path
+  end
+end
